e7e76a5e432cc1924709dedb3bcea3e4ba4ccaec
David Fernandez <daviferbri@gmail.com>
2026-09-04 17:26:54 -0600

    Realizadas funciones GET, POST, PUT y DELETE

--- services/api.service.js
+++ services/api.service.js
@@ -5,6 +5,9 @@
  * !! SUPER IMPROTANTE!!!
  * ! Todas las funciones asíncronas deben retornar una promesa
  * ! No improta que pienses tu qué está retornando, es una promesa
+ * 
+ * Nota:
+ * 
  */
 import { API_URL } from "../env.js";
 
@@ -16,20 +19,77 @@ export async function getAllProducts () {
     return data;
 }
 
-console.log(getAllProducts());
+//console.log(getAllProducts());
 
 // GET 1 solo producto
 export const getSingleProduct = async function(id){
  try{
     const response = await fetch(`${API_URL}/${id}`);
     const data = await response.json();
-    console.log(data);
+    return data;
  } catch(error){
     console.error("Error", error);
  }  
 }
 
-getSingleProduct(13);
-getSingleProduct(4);
-getSingleProduct(20);
+// POST 
+// Nota: Este método es el que se usa para ingresar información a la base de datos
+export const createProduct = async (productObject) => {
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(productObject)
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error", error);
+    }
+}
 
+// PUT
+export async function updateProduct (id, productObjectUpdate) {
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(productObjectUpdate)
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.error("Error", error);
+    }
+}
+
+// DELETE
+export async function deleteProduct (id){
+    try{
+        const response = await fetch(`${API_URL}/${id}`,{
+            method: "DELETE"
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.error("Error", error);
+    }
+}
+
+getSingleProduct(8);
+
+createProduct({
+    title: "chocolate",
+    price: 20,
+    description: "Extruido de cacao",
+    category: "Dulces",
+    image: "http://example.com"
+});
+
+updateProduct(13, {
+    "title": "Café",
+    "price": 200,
+    "description": "Café de Coateplatanos",
+});
+
+deleteProduct(10);
